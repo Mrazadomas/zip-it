5204fa11a1cb57a38a93ad113f7c8ef95d2be344
Mrazadomas <117606034+Mrazadomas@users.noreply.github.com>
2025-11-16 13:11:20 -0600

    done

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -6,9 +6,20 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test void test() {
+        App app = new App();
+        List<Integer> nums1 = List.of(1, 3, 5, 7);
+        List<Integer> nums2 = List.of(2, 4, 6, 8);
+        List<Integer> nums3 = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        assertEquals(app.zip(nums1, nums2), nums3);
+
+
+        List<String> colors1 = List.of("Red", "Green", "Blue");
+        List<String> colors2 = List.of("White", "Black", "Orange", "Pink", "Fuschia");
+        List<String> color3 = List.of("Red", "White", "Green", "Black", "Blue", "Orange", "Pink", "Fuschia");
+        assertEquals(app.zip(colors1, colors2), color3);
     }
 }
